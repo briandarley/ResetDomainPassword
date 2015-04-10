9f11d210617a7e2c015c5b171faa7796cba1d166
briandarley <bdarley@bdarley.net>
2015-04-10 10:15:56 -0400

    Initial Commit

---
+++ Reset Password/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.DirectoryServices.AccountManagement;
+
+namespace Reset_Password
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string accountName;
+                string password;
+
+                if (args == null || args.Length < 2)
+                {
+                    Console.WriteLine("Account Name (omit @gotropics.com)?");
+                    accountName = Console.ReadLine();
+                    Console.WriteLine("Password?");
+                    password = Console.ReadLine();
+                }
+                else
+                {
+                    accountName = args[0];
+                    password = args[1];
+                }
+                
+                if (string.IsNullOrEmpty(password))
+                {
+                    Console.WriteLine("Password is empty");
+                    Console.ReadLine();
+                    return;
+                }
+                accountName = accountName + "@gotropics.com";
+
+
+                using (var ctx = new PrincipalContext(ContextType.Domain))
+                {
+                    // find a user
+                    var user = UserPrincipal.FindByIdentity(ctx, accountName);
+
+                    if (user == null)
+                    {
+                        Console.WriteLine("User does not exist");
+                        return;
+                    }
+
+                    user.ChangePassword(password , "MuddButt1");
+                    Console.WriteLine("Resetting Password To: " + "MuddButt1");
+
+
+                    int i;
+                    for (i = 1; i < 20; i++)
+                    {
+                        user.ChangePassword("MuddButt" + i, "MuddButt" + (i + 1));
+                        Console.WriteLine("Resetting Password To: " + "MuddButt" + (i + 1));
+
+                    }
+
+                    user.ChangePassword("MuddButt" + i, password);
+                    Console.WriteLine("Password is now reset to " + password);
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
